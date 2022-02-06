71941d2ba2f4ad6def3d6bc9353cb8d7466ef719
Yualbe Luis <lois21us@gmail.com>
2022-02-06 16:31:41 +0100

    Autenticación

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // login True
+
+        // login False
+    }
+
+    public function validateLogin(Request $request)
+    {
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
-
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +21,5 @@ Route::apiResource('v1/posts', PostV1::class)->only(['index', 'show', 'destroy']
 
 // V2
 Route::apiResource('v2/posts', PostV2::class)->only(['index', 'show']);
+
+Route::post('login', [LoginController::class, 'login']);
